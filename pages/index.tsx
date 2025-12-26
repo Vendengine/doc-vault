ee0a34da94e6b2c6f2f680c8dec26ec77f0e20e3
Vendengine <stevenwirth62@gmail.com>
2025-12-26 11:47:31 -0600

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Doc Vault</h1>
+      <textarea
+        placeholder="Type something here..."
+        style={{ width: "100%", height: "200px" }}
+      />
+    </div>
+  );
+}
